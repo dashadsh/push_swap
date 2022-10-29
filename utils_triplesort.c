e4cbc3b9df594e9e0e4360266f42ea51b41eaef6
Daria Goremykina <d.goremykina@gmail.com>
2022-10-29 02:00:13 +0200

    c

--- utils_triplesort.c
+++ utils_triplesort.c
@@ -1,11 +1,11 @@
 #include "push_swap.h"
 
 /*
-1 3 2		-> rra -> 2 1 3 -> sa  -> 1 2 3
-2 1 3		-> sa  -> 1 2 3
-2 3 1		-> rra -> 1 2 3 
-3 1 2		-> ra  -> 1 2 3 
-3 2 1		-> ra  -> 2 1 3 -> sa  -> 1 2 3
+1 3 2 ->rra ->2 1 3 ->sa ->1 2 3
+2 1 3 ->sa  ->1 2 3
+2 3 1 ->rra ->1 2 3 
+3 1 2 ->ra  ->1 2 3 
+3 2 1 ->ra  ->2 1 3 ->sa ->1 2 3
 
 If value of the first number is highest => ra
 If value of the second number is highest => rra,
